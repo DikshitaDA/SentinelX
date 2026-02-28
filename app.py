08d13463cbf7887393ee74d8855abbd53018b470
DikshitaDA <dikshitaachari257@gmail.com>
2026-02-28 22:48:16 +0530

    Day 1 complete: Flask server running successfully

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app=Flask(__name__)
 def home():
     return "SentinLX is running"
 
-if __name__ == "__main __":
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+DB_NAME="sentinelx.db"
+
+def get_connection():
+    conn=sqlite3.connect(DB_NAME)
+    conn.row_factory=sqlite3.Row
+    return conn
+
+def crate_logs_table():
+    conn=get_connection()
+    cursor=conn.cursor()
+
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS logs(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   ip_address TEXT,
+                   endpoint TEXT,
+                   timestamp TEXT
+                   )
+                   """)
+    conn.commit()
+    conn.close()
